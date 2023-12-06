fc97eb26bcb169db088aa6c962867a18b8141b93
Algernon Soriano <alger.soriano@gmail.com>
2023-12-06 18:42:35 +1000

    cheese

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    num = 1
+    for dwarf in dwarves:
+        print(f"{num}. {dwarf}")
+        num += 1
+    return
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(elems):
+    summon = []
+    for elem in elems:
+        summon.append(f"{elem.title()}!")
+    return summon
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
 
-def find_the_cheese():
-    pass
+    return False
+
+def find_the_cheese(calls):
+    for call in calls:
+        if call == "cheddar":
+            return call
+    
